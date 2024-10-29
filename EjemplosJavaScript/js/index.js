47db3b844228bc5b364084b351ea055fef86ef5a
GuillermoAguiar <al45514@un.edu.mx>
2024-10-28 20:52:42 -0600

    Actualización
    Se ven ejemplos de JavaScript y se implementan a la página de Hamburguesas(sin acomodar).

---
+++ EjemplosJavaScript/js/index.js
@@ -0,0 +1,10 @@
+
+console.log("Hola Mundo desde mi archivo");
+
+alert("Esto es un mensaje para el mundo");
+
+console.log(5+360);
+console.warn("Estamos dando tips a los alumnos de IDAI");
+
+console.info("ESTO ES MUY IMPORTANTE");
+console.error("No debí estudiar psicología");
